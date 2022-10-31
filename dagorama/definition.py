e010c1512d30ec796b18aee891f3fa6d3aa0351a
Pierce Freeman <piercefreeman@gmail.com>
2022-10-31 10:35:49 -0700

    Simple implementation of arg capture and local runner

--- dagorama/definition.py
+++ dagorama/definition.py
@@ -4,16 +4,7 @@ from dataclasses import dataclass
 from uuid import UUID, uuid4
 from collections.abc import Callable
 from typing import TypeVar, ParamSpec, cast
-
-class DAGDefinition(ABC):
-    def __call__(self, *args, **kwargs):
-        self.entrypoint(*args, **kwargs)
-        # TODO: Return a promise of the entire dag being completed
-        return None
-
-    @abstractmethod
-    def entrypoint(self, *args, **kwargs):
-        pass
+from typing import Any
 
 @dataclass
 class DAGPromise:
@@ -21,6 +12,29 @@ class DAGPromise:
     A promise of a future DAG result
     """
     id: UUID
+
+    function_name: str
+
+    # Can include static values and other DAGPromises
+    # Technically these should not be "Any" but should be any object type that
+    # can be pickled / json encoded over the wire
+    # We should add a validation step to make sure this is true at call time
+    calltime_args: list["DAGPromise" | Any] = None
+    calltime_kwargs: dict[str, "DAGPromise" | Any] = None
+
+
+RUN_LOOP_PROMISES: list[DAGPromise] = []
+
+
+class DAGDefinition(ABC):
+    def __call__(self, *args, **kwargs) -> DAGPromise:
+        result_promise = self.entrypoint(*args, **kwargs)
+        # Cast as an actual result promise since this is what clients expect
+        return cast(DAGPromise, result_promise)
+
+    @abstractmethod
+    def entrypoint(self, *args, **kwargs):
+        pass
 
 
 T = TypeVar('T')
@@ -43,15 +57,22 @@ def dagorama(
     def decorator(func: Callable[P, T]) -> Callable[P, T]:
         #@wraps(func)
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
-            print("Call wrapper", args, kwargs)
+            # Determine if first argument is the class itself - if so we
+            # should ignore this within the DAG
+            if args and isinstance(args[0], DAGDefinition):
+                args = args[1:]
+
             # This function will have a result
-            # TODO: Queue in the DAG backend
+            # Queue in the DAG backend
+            promise = DAGPromise(uuid4(), func.__name__, args, kwargs)
+            RUN_LOOP_PROMISES.append(promise)
             #return func(*args, **kwargs)
             return cast(
                 # Wrong cast of types but we want the static typechecker to believe that the function
                 # is returning the actual value as specified by the client caller
                 # https://docs.python.org/3/library/typing.html#typing.ParamSpec
-                T, DAGPromise(uuid4())
+                T, promise,
             )
+        wrapper.original_fn = func
         return wrapper
     return decorator
--- sample_dag.py
+++ sample_dag.py
@@ -1,4 +1,6 @@
-from dagorama.definition import DAGDefinition, dagorama
+from dagorama.definition import DAGDefinition, dagorama, DAGPromise, RUN_LOOP_PROMISES
+from itertools import chain
+from typing import Any
 
 
 class CustomDag(DAGDefinition):
@@ -11,8 +13,9 @@ class CustomDag(DAGDefinition):
                           obj4 (3) --> obj6 (5)
     
     """
+    @dagorama()
     def entrypoint(self, number: int):
-        self.linear_continue(number)
+        return self.linear_continue(number)
 
     @dagorama()
     def linear_continue(self, number: int):
@@ -21,7 +24,7 @@ class CustomDag(DAGDefinition):
             self.loop_map(obj2+i)
             for i in range(2)
         ]
-        self.loop_consolidate(results)
+        return self.loop_consolidate(results)
 
     @dagorama()
     def loop_map(self, number: int):
@@ -43,4 +46,78 @@ dag_result = dag(1)
 # Add timeout to the resolution
 #await dag_result.resolve()
 
-print("Finished")
+# result identifier -> result payload
+dependencies_met = {}
+
+# Recursively resolves promises in the given structures they appear
+# Supports nested lists and dictionaries
+# Will also deposit found nested promises into the identified_promises list
+def resolve_promises(obj: Any, unresolved_promises: list[DAGPromise]) -> Any:
+    # Deal with iterables
+    if isinstance(obj, dict):
+        return {
+            key: resolve_promises(value, unresolved_promises)
+            for key, value in obj.items()
+        }
+    elif isinstance(obj, set):
+        return {
+            resolve_promises(value, unresolved_promises)
+            for value in obj
+        }
+    elif isinstance(obj, list):
+        return [
+            resolve_promises(value, unresolved_promises)
+            for value in obj
+        ]
+    elif isinstance(obj, tuple):
+        return tuple([
+            resolve_promises(value, unresolved_promises)
+            for value in obj
+        ])
+
+    # Otherwise we just have a raw value
+    if isinstance(obj, DAGPromise):
+        if obj.id in dependencies_met:
+            cached_value = dependencies_met[obj.id]
+            if isinstance(cached_value, DAGPromise):
+                # We need to resolve this promise
+                # TODO: Check for DAG legitimacy during build-up to avoid infinite loops
+                return resolve_promises(cached_value, unresolved_promises)
+            return cached_value
+        else:
+            unresolved_promises.append(obj)
+            return obj
+    else:
+        return obj
+
+while RUN_LOOP_PROMISES:
+    # We'll need to find these functions in the runtime based on the function name
+    next_item = RUN_LOOP_PROMISES.pop(0)
+
+    unresolved = []
+
+    resolved_args = [
+        resolve_promises(argument, unresolved)
+        for argument in next_item.calltime_args
+    ]
+    resolved_kwargs = {
+        key: resolve_promises(argument, unresolved)
+        for key, argument in next_item.calltime_kwargs.items()
+    }
+
+    if unresolved:
+        # Re-queue to the end
+        RUN_LOOP_PROMISES.append(next_item)
+        continue
+
+    # Run the defined function
+    # For now we hard-code the DAG but this should be dynamically instantiated as needed
+    result = getattr(dag, next_item.function_name).original_fn(dag, *resolved_args, **resolved_kwargs)
+    dependencies_met[next_item.id] = result
+
+unresolved = []
+final_result = resolve_promises(dag_result, unresolved)
+assert not unresolved
+assert final_result == 9
+
+print("DONE")
