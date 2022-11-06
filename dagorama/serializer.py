126172f17075749232fbcabcc766bc11d2fcdb64
Pierce Freeman <piercefreeman@gmail.com>
2022-11-05 17:40:38 -0700

    Add function registration and caching

--- dagorama/serializer.py
+++ dagorama/serializer.py
@@ -1,20 +1,47 @@
 
 from importlib import import_module
-from inspect import getmodulename, isclass
+from inspect import isclass
 from uuid import UUID
 
 from dagorama.definition import DAGDefinition, DAGInstance
-from typing import cast
 
 
 def function_to_name(func):
-    module_name = func.__globals__["__name__"]
-    class_path = func.__qualname__
+    if isclass(func):
+        module_name = getattr(func, "__module__")
+        class_path = getattr(func, "__name__")
+    elif isinstance(func, (DAGDefinition, DAGInstance)):
+        return function_to_name(func.__class__)
+    else:
+        module_name = func.__globals__["__name__"]
+        class_path = func.__qualname__
 
     return f"{module_name}:{class_path}"
 
 
+FUNCTION_CACHE = {}
+
+
+def register_function(func):
+    global FUNCTION_CACHE
+    FUNCTION_CACHE[function_to_name(func)] = func
+
+
 def name_to_function(name: str, instance_id: UUID):
+    """
+    Takes a function name and attempts to convert it into a local callable.
+    If the function is part of a class that hasn't already been registered as
+    via `register_function`, will attempt to instantiate it with no initial
+    parameters.
+
+    If your class needs to be instantiated with runtime variables, make sure you
+    register it before the worker begins executing.
+
+    """
+    global FUNCTION_CACHE
+    if name in FUNCTION_CACHE:
+        return FUNCTION_CACHE[name]
+
     # Can also try to sniff for function names throughout entire global namespace, but this
     # is still conditioned on name conventions
     package_name, fn_path = name.split(":")
@@ -25,18 +52,25 @@ def name_to_function(name: str, instance_id: UUID):
     except ModuleNotFoundError:
         raise ValueError(f"Could not import function with path: {name}")
 
-    for component in fn_path_components:
-        print(mod, component)
+    for i, component in enumerate(fn_path_components):
+        # Get class from cached globals, if possible
+        component_name = f"{package_name}:{'.'.join(fn_path_components[:i+1])}"
+        if component_name in FUNCTION_CACHE:
+            mod = FUNCTION_CACHE[component_name]
+            continue
+
         if not hasattr(mod, component):
             raise ValueError(f"Unable to resolve dagorama function: {name}")
 
         mod = getattr(mod, component)
 
         if isclass(mod) and issubclass(mod, DAGDefinition):
-            mod = DAGInstance(instance_id, mod()) # type: ignore
+            definition = mod()
+            mod = DAGInstance(instance_id, definition) # type: ignore
         elif isclass(mod):
-            print("MOD", mod)
             # Assume we can instantiate classes with no init arguments
             mod = mod()
+
+        register_function(mod)
 
     return mod
